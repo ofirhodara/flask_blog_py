4615451b5c54d0f819a921a38c82789d234b60f4
ofir hodara <ofir1682002@gmail.com>
2021-12-24 12:02:46 +0200

    add log in properly, and account page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,13 @@
 """
     all the import requeired to this packeage
 """
-
+  
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
+
 
 bcrypt = Bcrypt()
 app = Flask(__name__)
@@ -15,7 +16,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
+
+from flaskblog.models import User
+
 # handle the sessions
 login_manager = LoginManager(app)
+
+# where is the log in route
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=user_id).first()
 
 from flaskblog import routes
